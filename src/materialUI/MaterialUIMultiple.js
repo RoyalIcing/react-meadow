a38c9dac94371d6a9879e7c5375f4211d2bb84ae
Patrick Smith <patrick@burntcaramel.com>
2016-02-04 17:19:59 +1100

    Add shouldComponentUpdate to MaterialUIMultiple

--- src/materialUI/MaterialUIMultiple.js
+++ src/materialUI/MaterialUIMultiple.js
@@ -6,24 +6,41 @@ import CardActions from 'material-ui/lib/card/card-actions';
 import FlatButton from 'material-ui/lib/flat-button';
 import Divider from 'material-ui/lib/divider';
 
-export default function MaterialUIMultiple({ values, title, description, level, itemComponent: Item, onAdd, onRemoveAtIndex }) {
-  const items = values.map((value, index) => (
-    <div>
-      <Item value={ value } index={ index } />
-      <FlatButton label='Remove' onTouchTap={ () => onRemoveAtIndex(index) } />
-      <Divider />
-    </div>
-  ));
+export default React.createClass({
+  displayName: 'Meadow.MaterialUI.Multiple',
 
-  return (
-    <Card>
-      <CardHeader title={ title } subtitle={ description } style={{ height: 'auto', paddingBottom: 0 }} />
-      <CardText>
-        { items }
-      </CardText>
-      <CardActions>
-        <FlatButton label='Add' onTouchTap={ () => onAdd() } />
-      </CardActions>
-    </Card>
-  );
-}
+  shouldComponentUpdate(nextProps, nextState) {
+    const currentProps = this.props;
+
+    return (
+      nextProps.values != currentProps.values ||
+      nextProps.title !== currentProps.title ||
+      nextProps.description !== currentProps.description
+      //nextProps.itemComponent !== currentProps.itemComponent
+    );
+  },
+
+  render() {
+    const { values, title, description, level, itemComponent: Item, onAdd, onRemoveAtIndex } = this.props;
+
+    const items = values.map((value, index) => (
+      <div key={ index }>
+        <Item value={ value } index={ index } title={ title } description={ description } />
+        <FlatButton label='Remove' onTouchTap={ () => onRemoveAtIndex(index) } />
+        <Divider />
+      </div>
+    ));
+
+    return (
+      <Card>
+        <CardHeader title={ title } subtitle={ description } style={{ height: 'auto', paddingBottom: 0 }} />
+        <CardText>
+          { items }
+        </CardText>
+        <CardActions>
+          <FlatButton label='Add' onTouchTap={ () => onAdd() } />
+        </CardActions>
+      </Card>
+    );
+  }
+});
